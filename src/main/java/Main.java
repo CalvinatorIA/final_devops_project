2699fa4beb93c57befbf22350ff2f1ec53d98d55
CalvinatorIA <103223639+CalvinatorIA@users.noreply.github.com>
2025-12-11 06:03:53 -0500

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        final int WIDTH = 10;
+        final int LENGTH = 10;
+        final int DEPTH = 10;
+
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < LENGTH; j++) {
+                for (int k = 0; k < DEPTH; k++) {
+
+                    int l = i + j + k;
+                    if (l < 10) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print(l + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
